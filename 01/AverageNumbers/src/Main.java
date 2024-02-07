d353ce2db5f280c3e72ce328f62f43e21830b727
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2024-02-08 00:25:43 +0200

    AverageNumbers

---
+++ 01/AverageNumbers/src/Main.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main ( String[] args ) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader ( new InputStreamReader ( System.in ) );
+        System.out.println ( "Enter first number:" );
+        double number1 = Double.parseDouble ( bufferedReader.readLine ( ) );
+        System.out.println ( "Enter second number:" );
+        double number2 = Double.parseDouble ( bufferedReader.readLine ( ) );
+        System.out.println ( "Enter third number number:" );
+        double number3 = Double.parseDouble ( bufferedReader.readLine ( ) );
+        System.out.println ( (number1 + number2 + number3) / 3 );
+    }
+}
